93f74dd0f04a162b8566288a7b40d448f3403d41
jonathan-Ri <93490294+jonathan-Ri@users.noreply.github.com>
2023-11-29 15:20:09 -0300

    Update MyVisitor.py
    arreglado problema de paciente 0

--- MyVisitor.py
+++ MyVisitor.py
@@ -39,8 +39,36 @@ class MyVisitor(GramaticaVisitor):
 
     print("matriz antes:\n", matriz)
     matriz = self.infestar(espacio, capas, matriz)
-    print("Matriz luego", matriz)
+    print("Matriz luego :")
+    for t in matriz:
+      print(t, "\n")
     self.dibujarMatriz(matriz, capas)
+    return True
+
+  def visitRunC(self, ctx):
+    espacio = int(ctx.INT(0).getText())
+    capas = int(ctx.INT(1).getText())
+    iteraciones = int(ctx.INT(2).getText())
+    matriz = []
+    for p in range(0, capas):
+      capa = matriz_np = np.array([[0] * espacio] * espacio)
+      matriz.append(capa)
+    self.paciente0 = [
+        random.randint(0, espacio - 1),
+        random.randint(0, espacio - 1),
+        random.randint(0, capas - 1)
+    ]
+    matriz[self.paciente0[0]][self.paciente0[1]][self.paciente0[2]] = 2
+    print(self.paciente0)
+    print("matriz antes:\n", matriz)
+
+    for q in range(0, iteraciones):
+      matriz = self.infestar2(espacio, capas, matriz)
+    self.dibujarMatriz(matriz, capas)
+    print("Matriz luego de [", iteraciones, "] iteraciones\n")
+    for t in matriz:
+      print(t, "\n")
+
     return True
 
   def visitRunItStatemen(self, ctx):
@@ -63,7 +91,9 @@ class MyVisitor(GramaticaVisitor):
     for q in range(0, iteraciones):
       matriz = self.infestar(espacio, capas, matriz)
     self.dibujarMatriz(matriz, capas)
-    print("Matriz luego de [", iteraciones, "] iteraciones\n", matriz)
+    print("Matriz luego de [", iteraciones, "] iteraciones\n")
+    for t in matriz:
+      print(t, "\n")
 
     return True
 
@@ -82,19 +112,16 @@ class MyVisitor(GramaticaVisitor):
     for p in range(0, capas):
       capa = matriz_np = np.array([[0] * espacio] * espacio)
       matriz.append(capa)
-    self.paciente0 = [
-        random.randint(0, espacio - 1),
-        random.randint(0, espacio - 1),
-        random.randint(0, capas - 1)
-    ]
     matriz[self.paciente0[0]][self.paciente0[1]][self.paciente0[2]] = 2
     print(self.paciente0)
     print("matriz antes:\n", matriz)
 
     for q in range(0, iteraciones):
       matriz = self.infestar(espacio, capas, matriz)
+    print("Matriz luego de [", iteraciones, "] iteraciones\n")
+    for t in matriz:
+      print(t, "\n")
     self.dibujarMatriz(matriz, capas)
-    print("Matriz luego de [", iteraciones, "] iteraciones\n", matriz)
 
     return True
 
@@ -111,6 +138,18 @@ class MyVisitor(GramaticaVisitor):
     if infestado[2] > 0:
       vecinos.append([infestado[0], infestado[1], infestado[2] - 1])
     if infestado[2] < (capas - 1):
+      vecinos.append([infestado[0], infestado[1], infestado[2] + 1])
+    return vecinos
+
+  def vecinos2(self, infestado, espacio, capas):
+    vecinos = []
+    if infestado[1] > 0:
+      vecinos.append([infestado[0], infestado[1] - 1, infestado[2]])
+    if infestado[1] < (espacio - 1):
+      vecinos.append([infestado[0], infestado[1] + 1, infestado[2]])
+    if infestado[2] > 0:
+      vecinos.append([infestado[0], infestado[1], infestado[2] - 1])
+    if infestado[2] < (espacio - 1):
       vecinos.append([infestado[0], infestado[1], infestado[2] + 1])
     return vecinos
 
@@ -134,33 +173,56 @@ class MyVisitor(GramaticaVisitor):
             matriz[x][y][k] = matriz[x][y][k] + 1
     return matriz
 
+  def infestar2(self, espacio, capas, matriz):
+    expuestosNuevos = []
+    for i in range(0, espacio):
+      for j in range(0, espacio):
+        for z in range(0, capas):
+          if matriz[i][j][z] == 2:
+            infestado = [i, j, z]
+            vecinos = self.vecinos2(infestado, espacio, capas)
+            for y in range(0, len(vecinos)):
+              if matriz[vecinos[y][0]][vecinos[y][1]][vecinos[y][2]] == 0:
+                matriz[vecinos[y][0]][vecinos[y][1]][vecinos[y][2]] = 1
+                expuestosNuevos.append(
+                    [vecinos[y][0], vecinos[y][1], vecinos[y][2]])
+    for x in range(0, espacio):
+      for y in range(0, espacio):
+        for k in range(0, capas):
+          if ([x, y, k] not in expuestosNuevos) and (matriz[x][y][k] != 0):
+            matriz[x][y][k] = matriz[x][y][k] + 1
+    return matriz
+
   def dibujarMatriz(self, matriz, capas):
-  # Define un nuevo colormap personalizado
-  custom_cmap = plt.cm.colors.ListedColormap(['white', 'green', 'yellow', '#FFDAB9', '#FFA500', '#8B4513'])
-  num_matrices = len(matriz)
-  if (num_matrices == 1):
+    # Define un nuevo colormap personalizado
+    custom_cmap = plt.cm.colors.ListedColormap(
+        ['white', 'green', 'yellow', '#FFDAB9', '#FFA500', '#8B4513'])
+    num_matrices = len(matriz)
+    if (num_matrices == 1):
       plt.imshow(matriz[0], cmap=custom_cmap, interpolation='nearest')
       plt.colorbar()
       plt.show()
-  elif (num_matrices == 2):
+    elif (num_matrices == 2):
       fig, axs = plt.subplots(1, 2, figsize=(15, 5))
       axs[0].imshow(matriz[0], cmap=custom_cmap, interpolation='nearest')
       axs[0].set_title('Matriz 1')
       axs[1].imshow(matriz[1], cmap=custom_cmap, interpolation='nearest')
       axs[1].set_title('Matriz 2')
       plt.show()
-  else:
+    else:
       num_filas = int(np.ceil(np.sqrt(num_matrices)))
       num_columnas = int(np.ceil(num_matrices / num_filas))
       fig, axs = plt.subplots(num_filas, num_columnas, figsize=(4, 4))
       for i in range(0, num_matrices):
-          fila = i // num_columnas
-          columna = i % num_columnas
-          axs[fila, columna].imshow(matriz[i], cmap=custom_cmap, interpolation='nearest')
-          axs[fila, columna].set_title(f'Matriz {i + 1}')
+        fila = i // num_columnas
+        columna = i % num_columnas
+        axs[fila, columna].imshow(matriz[i],
+                                  cmap=custom_cmap,
+                                  interpolation='nearest')
+        axs[fila, columna].set_title(f'Matriz {i + 1}')
       plt.tight_layout()
       plt.show()
-  return True
+    return True
 
   def visitApagar(self, ctx):
     return False
